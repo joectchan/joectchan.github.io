e95832302159f1273a8c9eea56c0a432a4c85eb5
joectchan <jctchan@gmail.com>
2020-05-08 11:39:21 -0700

    Create 2020_05_08_plain_md_post.d

---
+++ _posts/2020/05/08/2020_05_08_plain_md_post.d
@@ -0,0 +1,12 @@
+---
+title: "Post: Quote"
+categories:
+  - Blog
+tags:
+  - Post Formats
+  - quote
+---
+
+> In this world nothing can be said to be certain, except death and taxes.
+
+> <cite><a href="https://www.brainyquote.com/quotes/benjamin_franklin_129817>Benjamin Franklin</a></cite>
